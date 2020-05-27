953d9b3d106438b5b6df23876ad5e1f070b2bff1
JackWilliam <2013021287@cuit.edu.cn>
2020-05-27 19:28:11 +0800

    5.27

---
+++ Middle/Failed/Leetcode_Fail_Middle_199_binary_tree_right_side_view.cpp
@@ -0,0 +1,102 @@
+/* 
+   Leecode 199 binary tree right side view
+   Level: Middle
+
+   Author: JackWilliam
+   Date: 27, May, 2020
+*/
+
+/*
+    Solution
+    Key Point: DFS  用栈  
+    在搜索过程中，我们总是先访问右子树。那么对于每一层来说，我们在这层见到的第一个结点一定是最右边的结点
+
+    Source: LeetCode
+    Author: LeetCode-Solution
+    Url: https://leetcode-cn.com/problems/binary-tree-right-side-view/solution/er-cha-shu-de-you-shi-tu-by-leetcode-solution/
+*/
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        unordered_map<int, int> rightmostValueAtDepth;
+        int max_depth = -1;
+
+        stack<TreeNode*> nodeStack;
+        stack<int> depthStack;
+        nodeStack.push(root);
+        depthStack.push(0);
+
+        while (!nodeStack.empty()) {
+            TreeNode* node = nodeStack.top();nodeStack.pop();
+            int depth = depthStack.top();depthStack.pop();
+
+            if (node != NULL) {
+            	// 维护二叉树的最大深度
+                max_depth = max(max_depth, depth);
+
+                // 如果不存在对应深度的节点我们才插入
+                if (rightmostValueAtDepth.find(depth) == rightmostValueAtDepth.end()) {
+                    rightmostValueAtDepth[depth] =  node -> val;
+                }
+
+                nodeStack.push(node -> left);
+                nodeStack.push(node -> right);
+                depthStack.push(depth + 1);
+                depthStack.push(depth + 1);
+            }
+        }
+
+        vector<int> rightView;
+        for (int depth = 0; depth <= max_depth; ++depth) {
+            rightView.push_back(rightmostValueAtDepth[depth]);
+        }
+
+        return rightView;
+    }
+};
+
+/*
+    Solution
+    Key Point: BFS   用队列  
+    对二叉树进行层次遍历，那么对于每层来说，最右边的结点一定是最后被遍历到的。二叉树的层次遍历可以用广度优先搜索实现。
+    Source: LeetCode
+    Author: LeetCode-Solution
+    Url: https://leetcode-cn.com/problems/binary-tree-right-side-view/solution/er-cha-shu-de-you-shi-tu-by-leetcode-solution/
+*/
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        unordered_map<int, int> right2mostValueAtDepth;
+        int max_depth = -1;
+
+        queue<TreeNode*> nodeQueue;
+        queue<int> depthQueue;
+        nodeQueue.push(root);
+        depthQueue.push(0);
+
+        while (!nodeQueue.empty()) {
+            TreeNode* node = nodeQueue.front();nodeQueue.pop();
+            int depth = depthQueue.front();depthQueue.pop();
+
+            if (node != NULL) {
+            	// 维护二叉树的最大深度
+                max_depth = max(max_depth, depth);
+
+                // 由于每一层最后一个访问到的节点才是我们要的答案，因此不断更新对应深度的信息即可
+                rightmostValueAtDepth[depth] =  node -> val;
+
+                nodeQueue.push(node -> left);
+                nodeQueue.push(node -> right);
+                depthQueue.push(depth + 1);
+                depthQueue.push(depth + 1);
+            }
+        }
+
+        vector<int> rightView;
+        for (int depth = 0; depth <= max_depth; ++depth) {
+            rightView.push_back(rightmostValueAtDepth[depth]);
+        }
+
+        return rightView;
+    }
+};
